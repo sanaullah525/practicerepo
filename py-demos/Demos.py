8b4427d3287bf036927ccc6a17f71dbac5495dbe
sanaullah525 <sanasanaullah524@gmail.com>
2026-04-10 15:37:32 +0500

    Added code to Demo.py file.

--- py-demos/Demos.py
+++ py-demos/Demos.py
@@ -4,6 +4,35 @@
   and basic arithmetic operations.
 """
 
+
+"""
+ demo.py
+ Simple Python scripts to demonstrate print statements
+  and basic arithmetic operations.
+"""
+
+
+"""
+print("Welcome to the python world !!!", 
+      "This is my first demo script.",
+    "Python is fun to learn and use.", "Happy coding!", 
+        "Let's explore more Python features.")
+
+#  Time for some arithmetic operations using python.
+#  Addition, Subtraction, Multiplication, Division, 
+# ... Integer Division, Modulus, Exponentiation
+print("This is to print some arithmetics using python. ")
+print("21+4= ", 21+4)
+print("Multiplication: 7*6= ", 7*6)
+print("Division: 56/8= ", 56/8)
+print("Subtraction: 45-9= ", 45-9)
+print("Integer Division: 22//7= ", 22//7)
+print("Modulus: 29%5= ", 29%5)
+print("Exponentiation: 3**2= ", 3**2)
+print("That's all for now. More to come soon...")
+"""
+
+
 """
 print("Welcome to the python world !!!", 
       "This is my first demo script.",
@@ -814,4 +843,6 @@ print("Name:", name)
 print("Status:", status)
 
 """
+
+
 
