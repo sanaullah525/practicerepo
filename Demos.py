375ed12ca5db1b99938f123da841024e0abc9c83
sanaullah525 <sanasanaullah524@gmail.com>
2026-05-12 16:43:28 +0500

    Coursera labs. Using iloc to return a DataFrame view of the data in rows 0,1,2 at column 3 and 4.

--- Demos.py
+++ Demos.py
@@ -2127,5 +2127,5 @@ print(df.horsepower, "\n")  # A column can be selected by name using a dot notat
 print(df[["make", "num_of_doors", "city_mpg", "price"]], "\n")    # You can create a dataframe of specific columns 
 # using a list of column names inside brackets.
 print(dfm.iloc[1], "\n")      # Use .iloc() to return a Series object of the data in row 0.
-
 print(dfm.iloc[0:3], "\n")  # Using .iloc() to return a DataFrame view of the data in rows 0,1,2
+print(dfm.iloc[0:3, [3,4]], "\n") # Using .iloc() to return a DataFrame view of the data in rows 0,1,2 at colum 3 and 4.
