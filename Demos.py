92d94843ad3e0abc79fdc03645b2e0f3990536a2
sanaullah525 <sanasanaullah524@gmail.com>
2026-05-13 00:26:50 +0500

    Coursera lab. Instantiation of new data for Grouping and aggregation in python.

--- Demos.py
+++ Demos.py
@@ -2178,3 +2178,18 @@ print(hardmsk, "\n")
 
 import numpy as np
 import pandas as pd
+
+
+# Instantiate a dictionary of planetry data.
+planetsdata= {
+    "planet": ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
+    "radius_km": [2440, 6052, 6371, 3390, 69911, 58232, 25362, 24622],
+    "moons": [0, 0, 1, 2, 80, 83, 27, 14],
+    "type": ['terrestrial', 'terrestrial', 'terrestrial', 'terrestrial',
+                 'gas giant', 'gas giant', 'ice giant', 'ice giant'],
+    'rings': ['no', 'no', 'no', 'no', 'yes', 'yes', 'yes','yes'],
+    'mean_temp_c': [167, 464, 15, -65, -110, -140, -195, -200],
+    'magnetic_field': ['yes', 'no', 'yes', 'no', 'yes', 'yes', 'yes', 'yes']
+}
+print(planetsdata, "\n")
+print(pd.DataFrame(planetsdata), "\n")
