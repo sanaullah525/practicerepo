7acbeac01b9f13221dc19d25719e636b0daefa56
sanaullah525 <sanasanaullah524@gmail.com>
2026-06-25 00:30:50 +0500

    Exception handling in python.

--- Demos.py
+++ Demos.py
@@ -2317,3 +2317,12 @@ eggs= 42
 spam()
 print(eggs)
 """
+
+
+# Exception Handling in python.
+def spam(divideBy):
+    return 42 / divideBy
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
