9efe02380cc8e2a0c5cfa954236a566d5b95f06d
sanaullah525 <sanasanaullah524@gmail.com>
2026-07-13 23:44:17 +0500

    star pattern.

--- Demos.py
+++ Demos.py
@@ -2866,10 +2866,22 @@ for x in range(6):
 
 
 
-#_______________________________________________
+#__________________________________________
 #   Pattern programing questions in python.
-#```````````````````````````````````````````````
+#``````````````````````````````````````````
 
 # sp stands for print star pattern.
 # isp stands for inverted star pattern.
+
+def sp():
+  n= int(input("Enter number of rows: "))
+  print()
+
+  for i in range(n):
+    for j in range(i+1):
+      print("*", end= " ")
+    print()
+  print("\n")
+sp()
+
 
