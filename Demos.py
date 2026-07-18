e34a08d7731401dbd8bd30f1959931a5f1c34937
sanaullah525 <sanasanaullah524@gmail.com>
2026-07-19 01:46:31 +0500

    CharacterCounter functionality and how it avoids throwing errors.

--- Demos.py
+++ Demos.py
@@ -3110,6 +3110,11 @@ print(pet, "\n\n")
 
 
 # CharacterCount
-message= 'It was bright cold day in April, and the clocks were striking thirteen.'
+message= 'It was a bright cold day in April, and the clocks were striking thirteen.'
 count= {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character]= count[character] + 1
+print(count, "\n")
 
