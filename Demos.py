c301e9c4d4d51e569ae3a00629968e884c183c84
sanaullah525 <sanasanaullah524@gmail.com>
2026-07-20 00:05:19 +0500

    Adding spacing lines..

--- Demos.py
+++ Demos.py
@@ -3167,3 +3167,5 @@ def printBoard(board):
     print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
     print('-+-+-')
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+printBoard(board)
+
