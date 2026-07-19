a7a75675790db9c2c92530b64ff15d177f8a38f6
sanaullah525 <sanasanaullah524@gmail.com>
2026-07-20 01:33:24 +0500

    Adding a board in which players can give input.

--- Demos.py
+++ Demos.py
@@ -3174,3 +3174,21 @@ printBoard(oboard); print("\n\n")
 
 
 # Allowing the players to enter their moves.
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+turn= "X"
+for i in range(9):
+    printBoard(board)
+    print('Turn for ' + turn + '. Move on which space?')
+    move= input()
+    board[move]= turn
+    if turn == 'X':
+        turn= 'O'
+    else:
+        turn= 'X'
+printBoard(board)
+
