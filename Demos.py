dec15007010cb4a55501db9ab21f5c8167ebd923
sanaullah525 <sanasanaullah524@gmail.com>
2026-07-20 00:04:57 +0500

    printing board as a function.

--- Demos.py
+++ Demos.py
@@ -3161,3 +3161,9 @@ print(oboard, "\n")
 
 
 #___Printing board as a function.
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
