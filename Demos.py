8a06f0c6954c7cba17472c9baaf933ead287e2c2
sanaullah525 <sanasanaullah524@gmail.com>
2026-07-21 02:04:12 +0500

    Capitalizing nouns.

--- Demos.py
+++ Demos.py
@@ -3205,9 +3205,9 @@ for role, count in user_counts.items():
 #    Using Data Structures to Model Real-World Things.
 #``````````````````````````````````````````````````````
 
-allGuests= {'Alice' : {'apples' : 5, 'pretzels' : 12},
-            'Bob' : {'ham sandwiches' : 3, 'apples' : 2},
-            'Carol': {'cups': 3, 'apple pies': 1}
+allGuests= {'Alice' : {'Apples' : 5, 'Pretzels' : 12},
+            'Bob' : {'Ham sandwiches' : 3, 'Apples' : 2},
+            'Carol': {'Cups': 3, 'Apple Pies': 1}
             }
 
 def totalBrought(guests, item):
