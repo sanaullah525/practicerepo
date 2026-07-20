8778dfa0664a2a6e062cd32af5480fcefaca5e22
sanaullah525 <sanasanaullah524@gmail.com>
2026-07-20 23:56:56 +0500

    printing Nested dictionaries.

--- Demos.py
+++ Demos.py
@@ -3202,3 +3202,4 @@ allGuests= {'Alice' : {'apples' : 5, 'pretzels' : 12},
             'Bob' : {'ham sandwiches' : 3, 'apples' : 2},
             'Carol': {'cups': 3, 'apple pies': 1}
             }
+print(allGuests)
