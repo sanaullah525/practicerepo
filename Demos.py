d8b60cd09dd484eb13f532a1570aff22a4baf30f
sanaullah525 <sanasanaullah524@gmail.com>
2026-07-21 00:48:51 +0500

    totalBrought function.

--- Demos.py
+++ Demos.py
@@ -3202,4 +3202,3 @@ allGuests= {'Alice' : {'apples' : 5, 'pretzels' : 12},
             'Bob' : {'ham sandwiches' : 3, 'apples' : 2},
             'Carol': {'cups': 3, 'apple pies': 1}
             }
-print(allGuests)
